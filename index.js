ef35bea668b3bd11a5d2a95a87dc7ff1d8e888c0
Kosi Akporji <kosiazom@gmail.com>
2020-08-20 15:08:33 -0400

    did the range and difficulities

--- index.js
+++ index.js
@@ -1,16 +1,62 @@
 let computerGuess;
-const gameArea = document.getElementById('gameArea')
+let userGuesses = [];
+const gameArea = document.getElementById('gameArea');
+let attempts = 0;
+let maxGuesses;
+
+let low = 1;
+let high = 100; 
+
 
 document.addEventListener("DOMContentLoaded", (e) => {
     console.log("The DOM is here!")
-    init()
-    easyMode()
-    hardMode()
+    init();
+    easyMode();
+    hardMode();
+    compareGuess();
+    newGame()
+    
 })
 
+function updateRange() {
+const rangeOutput = document.getElementById('rangeOutput')
+rangeOutput.innerText= `${low} - ${high}`;
+rangeOutput.style.marginLeft =  low + '%';
+rangeOutput.style.marginRight = 100 - high + '%';
+rangeOutput.classList.add('flash')
+
+
+const lowValue = document.getElementById('low')
+
+lowValue.style.flex = low + '%';
+lowValue.style.background = "#ef7b54"
+
+
+const space = document.getElementById('space')
+space.style.flex = high - low + '%';
+space.style.background = "#83e1d0"
+
+const highValue = document.getElementById('high')
+highValue.style.flex = 100 - high + '%';
+highValue.style.background = "#ef7b54"
+}
+
+function gameEnded() {
+    let newGame = document.getElementById('newGameButton');
+    newGame.style.display = "inline";
+    document.getElementById("inputBox").setAttribute('readonly', 'readonly')
+}
+
+function newGame() {
+    let newGameButton = document.getElementById('newGameButton');
+    newGameButton.addEventListener('click', (e) => {
+        window.location.reload();
+    })
+}
 function init() {
-    computerGuess = Math.floor(Math.random() * 100 + 1)
-  let newGame = document.getElementById('newGameButton')
+    computerGuess = Math.floor(Math.random() * 100 + 1);
+    console.log(computerGuess);
+  let newGame = document.getElementById('newGameButton');
   newGame.style.display = "none"
 
 //   let gameArea = document.getElementById('gameArea')
@@ -18,6 +64,7 @@ function init() {
 }
 
 function startGameView() {
+    
     let start = document.getElementById('welcomeScreen')
     start.style.display = "none"
 
@@ -26,16 +73,83 @@ function startGameView() {
 }
 
 function easyMode() {
+  
 let easy = document.getElementById('easy')
 easy.addEventListener('click', (e) => {
+    maxGuesses = 10;
     startGameView()
-})
+    })
 }
 
 function hardMode() {
+      
     let hard = document.getElementById('hard')
     hard.addEventListener('click',(e) => {
+        maxGuesses = 5;
         startGameView();
+    
     })
+}
 
+function compareGuess() {
+    let userInput = document.getElementById("inputBox")
+    userInput.addEventListener("change", (e)=> {
+        let userInputValue = Number(userInput.value);
+      
+        userGuesses.push(" " + userInputValue);
+        console.log(userGuesses);
+        let guesses = document.getElementById("guesses");
+        guesses.innerHTML = userGuesses;
+        console.log(guesses)
+       document.getElementById('attempts').innerHTML = attempts +=1
+
+
+        if(attempts < maxGuesses) {
+          
+            if (userInputValue > computerGuess) {
+                if (userInputValue < high)  high = userInputValue
+            let yourGuess = document.getElementById("textOutput");
+            yourGuess.innerHTML = " Your guess is too high";
+            document.getElementById("inputBox").value = "";
+        }
+
+        else if(userInputValue < computerGuess) {
+            if (userInputValue > low) low = userInputValue;
+            let yourGuess = document.getElementById("textOutput");
+            yourGuess.innerHTML = " Your guess is too low";
+            document.getElementById("inputBox").value = "";
+
+        }
+
+        else {
+            let yourGuess = document.getElementById("textOutput");
+            yourGuess.innerHTML = "Correct! You Got it in " + attempts + " attempts!";
+            gameEnded();
+        }
+    }
+    else {
+        if (userInputValue > computerGuess) {
+            let yourGuess = document.getElementById("textOutput");
+            yourGuess.innerHTML = " YOU LOSE!, <br> The number was " + computerGuess;
+            gameEnded();
+          
+        }
+
+        else if(userInputValue < computerGuess) {
+            let yourGuess = document.getElementById("textOutput");
+            yourGuess.innerHTML = " YOU LOSE!, <br> The number was " + computerGuess;
+            gameEnded();
+          
+        }
+
+        else {
+            let yourGuess = document.getElementById("textOutput");
+            yourGuess.innerHTML = "Correct! You Got it in " + attempts + " attempts!";
+            gameEnded();
+        }
+    }
+    updateRange()
+})
+
+    
 }
